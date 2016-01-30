4832bc4a25e76e8dc4311dcb7662bd2013971ba5
Matthias Brandt <matthias.brandt@inoio.de>
2016-01-30 15:31:19 +0100

    Link logs dir to host.

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -14,9 +14,11 @@ mkdir -p $SSL_FOLDER
 rm /etc/nginx/sites-available/default
 rm $SSL_FOLDER/ssl-test.matthias-brandt.de.crt
 rm $SSL_FOLDER/ssl-test.matthias-brandt.de.key
+rm -rf /var/log/nginx
 
 ln -s $SHARED_FOLDER/nginx-config                    /etc/nginx/sites-available/default
 ln -s $SHARED_FOLDER/ssl-test.matthias-brandt.de.crt $SSL_FOLDER/ssl-test.matthias-brandt.de.crt
 ln -s $SHARED_FOLDER/ssl-test.matthias-brandt.de.key $SSL_FOLDER/ssl-test.matthias-brandt.de.key
+ln -s $SHARED_FOLDER/logs                            /var/log/nginx
 
 service nginx restart
